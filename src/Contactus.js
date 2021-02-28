292a251c2146a40b24d443e0dd3c6b765c0f0f57
Muhammad Nauman <nouman41307@github.com>
2021-02-28 23:47:35 +0500

    added

---
+++ src/Contactus.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Container, Col, Row, Modal } from "react-bootstrap";
+import "./Style/Shop.css";
+
+
+function Contactus() {
+
+return(
+<div>
+<div class="container-fluid dv1">
+
+
+
+
+</div>
+
+
+
+
+<div class="blogbanner">
+    <img class="bannerimg" src="Content/PNG/Web Banner 03.png"/>
+
+    <div class="bannertext2">
+        BabaBeard - Contact
+    </div>
+
+    <div>
+        <img class="blogbannerimgmustache" src="Content/PNG/Mustache.png"/>
+
+    </div>
+ 
+</div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 col-sm-12 col-md-12">
+            <div class="policymainheading">
+                Contact Us
+            </div>
+
+            <div class="policyheading" style="text-align: center;">
+                Got any query?
+
+            </div>
+            <div class="policycontent" style="text-align: center;">
+                You can contact us at <a href="Support@bababeard.com">Support@bababeard.com </a> or leave your message on
+                BabaBeard.bb@gmail.com our team will respond to your query as quickly as
+                possible.
+                Don’t forget to connect with us on facebook at Baba Beard
+                Instagram at Baba beard
+                And twitter at Baba Beard.
+            </div>
+           
+
+        </div>
+    </div>
+    
+</div>
+
+</div>
+
+);
+
+}
+export default Contactus;
+
+   
